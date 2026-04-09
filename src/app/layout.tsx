92398b462174eaffae930f13e68547f0e37db5e3
Jason Ramirez <raiderj77@gmail.com>
2026-04-08 21:37:36 -0700

    feat: add GA4 tracking G-YCXZXYNF14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,14 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google Analytics GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YCXZXYNF14"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-YCXZXYNF14');`}
+        </Script>
         {/* Microsoft Clarity */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","vsqobt7va0");`}
